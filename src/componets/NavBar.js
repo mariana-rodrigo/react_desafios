96a06645484a9b7ec75a7e968eea705c1ae5ff83
Mariana Rodrigo <marianarodrigo90@gmail.com>
2021-07-04 20:12:36 -0300

    desafío 2

---
+++ src/componets/NavBar.js
@@ -0,0 +1,25 @@
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        
+        <nav>
+            
+                <div>
+                    
+                    <ul className="listaNav">
+                        <li className="elementoListaNav">Inicio</li> 
+                        <li className="elementoListaNav">Productos</li>
+                        <li className="elementoListaNav">Galería</li>
+                        <li className="elementoListaNav">Carrito</li>
+                        
+                        
+                    </ul>
+                </div>
+            
+        </nav>
+    
+    );
+}
+
+export default NavBar;
